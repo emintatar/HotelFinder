213c2b90e2f5fc055996c270bada8b8c0c8c3554
Emin Tatar <emintatar48@outlook.com>
2023-05-18 16:43:12 +0300

    first commit

---
+++ HotelFinder.DataAccess/HotelDbContext.cs
@@ -0,0 +1,20 @@
+﻿using HotelFinder.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.DataAccess
+{
+    public class HotelDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=HotelDb;Trusted_Connection=true");
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+    }
+}
